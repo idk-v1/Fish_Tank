70a96e2d7d754d5f424beca7628afe479fd290c4
idk-v1 <benhamilton.jpg@gmail.com>
2022-08-07 19:02:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var School = new Array();
 var Bubbles = new Array(20).fill(new Bubble());
 
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 10; i++) {
     School[i] = new Fish();
 }
 
+var body = document.querySelector("body");
+body.style = "background-image: linear-gradient(180deg, #e66465, #9198e5);";
+//body.style = "background: #00f";
+
 var dlt = 0;
 var lst = performance.now();
-update();
+for (var i = 0; i < 1000; i++) {
+    update();
+}
 var int = setInterval(tick, 0);
 
 function tick() {
@@ -22,27 +28,51 @@ function tick() {
 }
 
 function render() {
+    for (var f = 0; f < School.length; f++) {
 
+        School[f].e.style.position = "fixed";
+        School[f].e.style.left = (window.innerWidth / 2 + School[f].d * window.innerWidth / 1.5 - School[f].x - 100) + "px";
+        School[f].e.style.top = (School[f].y - 7.5) + "%";
+        School[f].e.style.width = "200px";
+        School[f].e.style.height = "100px";
+        School[f].e.style.background = "rgb(" + School[f].r + "," + School[f].g + "," + School[f].b + ")";
+        School[f].e.style.borderRadius = "25%";
+
+        if (School[f].d == 1) {
+            School[f].efm.style = "position: absolute; left: 12.5%; top: 12.5%; width: 50px; height: 25px; border-top: 25px solid transparent; border-right: 50px solid #00000080; border-bottom: 25px solid transparent";
+            School[f].efb.style = "position: absolute; left: 75%; top: 12.5%; width: 50px; height: 25px; border-top: 25px solid transparent; border-right: 38px solid rgb(" + School[f].r / 2 + "," + School[f].g / 2 + "," + School[f].b / 2 + "); border-bottom: 25px solid transparent;";
+            School[f].eft.style = "position: absolute; left: 37.5%; top: -25%; width: 50px; height: 25px; background: rgb(" + School[f].r / 2 + "," + School[f].g / 2 + "," + School[f].b / 2 + "); transform: skew(-20deg);";
+            School[f].ee.style = "position: absolute; left: 12.5%; top: 25%; width: 7.5px; height: 7.5px; background-color: #000; padding: 7.5px; border: solid 5px #fff; border-radius: 50%;";
+        } else {
+            School[f].efm.style = "position: absolute; right: 12.5%; top: 12.5%; width: 50px; height: 25px; border-top: 25px solid transparent; border-left: 50px solid #00000080; border-bottom: 25px solid transparent";
+            School[f].efb.style = "position: absolute; right: 75%; top: 12.5%; width: 50px; height: 25px; border-top: 25px solid transparent; border-left: 38px solid rgb(" + School[f].r / 2 + "," + School[f].g / 2 + "," + School[f].b / 2 + "); border-bottom: 25px solid transparent;";
+            School[f].eft.style = "position: absolute; right: 37.5%; top: -25%; width: 50px; height: 25px; background: rgb(" + School[f].r / 2 + "," + School[f].g / 2 + "," + School[f].b / 2 + "); transform: skew(20deg);";
+            School[f].ee.style = "position: absolute; right: 12.5%; top: 25%; width: 7.5px; height: 7.5px; background-color: #000; padding: 7.5px; border: solid 5px #fff; border-radius: 50%;";
+        }
+    }
 }
 
 function update() {
     for (var f = 0; f < School.length; f++) {
-        School[f].e.id = f;
-        School[f].e.style.position = "fixed";
-        School[f].e.style.left = (window.innerWidth / 2 + School[f].d * window.innerWidth / 2 - School[f].x - 100) + "px";
-        School[f].e.style.top = School[f].y + "%";
-        School[f].e.style.width = "200px";
-        School[f].e.style.height = "100px";
-        School[f].e.style.background = "rgb(" + School[f].r + "," + School[f].g + "," + School[f].b + ")";
-        document.querySelector("body").appendChild(School[f].e);
+
         School[f].x += School[f].s * School[f].d;
-        if (School[f].d == 1 && School[f].x >= window.innerWidth) {
+        if (School[f].d == 1 && School[f].x >= window.innerWidth + 300) {
             School[f].e.remove();
             School[f] = new Fish();
+            School[f].e.appendChild(School[f].efm);
+            School[f].e.appendChild(School[f].efb);
+            School[f].e.appendChild(School[f].eft);
+            School[f].e.appendChild(School[f].ee);
+            document.querySelector("body").appendChild(School[f].e);
         }
-        if (School[f].d == -1 && School[f].x <= -window.innerWidth) {
+        if (School[f].d == -1 && School[f].x <= -window.innerWidth - 300) {
             School[f].e.remove();
             School[f] = new Fish();
+            School[f].e.appendChild(School[f].efm);
+            School[f].e.appendChild(School[f].efb);
+            School[f].e.appendChild(School[f].eft);
+            School[f].e.appendChild(School[f].ee);
+            document.querySelector("body").appendChild(School[f].e);
         }
     }
 }
@@ -56,6 +86,10 @@ function Fish() {
     this.g = Math.floor(Math.random() * 256);
     this.b = Math.floor(Math.random() * 256);
     this.e = document.createElement("div");
+    this.efm = document.createElement("div");
+    this.efb = document.createElement("div");
+    this.eft = document.createElement("div");
+    this.ee = document.createElement("div");
 }
 
 function Bubble() {
